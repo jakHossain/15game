a1361f8e78ec424a96aa1d403e8814a7898583f6
Jakir Hossain <js.hossain256@gmail.com>
2024-04-25 17:28:50 -0400

    Added user input logic for commands

---
+++ UserInput.h
@@ -0,0 +1,47 @@
+#ifndef USERINPUTGUARD
+#define USERINPUTGUARD
+
+#include <iostream>
+#include <limits>
+
+namespace UserInput
+{
+	void cleanInput()
+	{
+		std::cout << "Invalid! Try again.\n";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+	char getCommandFromUser()
+	{
+		while (true)
+		{
+			std::cout << "Enter a move: ";
+			char input{};
+			std::cin >> input;
+
+
+			if (!std::cin)
+			{
+				cleanInput();
+				continue;
+			}
+
+			switch (input)
+			{
+			case 'w':
+			case 'a':
+			case 'd':
+			case 's':
+			case 'q':
+				return input;
+			default:
+				cleanInput();
+				continue;
+			}
+		}
+
+	}
+}
+
+#endif // !USERINPUTGUARD
--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include "Tile.h"
 #include "Board.h"
+#include "UserInput.h"
 
 extern const int g_consoleLines{ 25 };
 
 int main()
 {
 	Board b{};
-	Tile t{ 12 };
-	Tile t2{ 10 };
-	Tile t3{ 3 };
-	Tile t0{ 0 };
-	std::cout << "123456789ABCDEF\n";
-	std::cout << t0 << t << t2 << t3 <<  "\n";
-
-	std::cout << "Tile one has the number: " << t.getNum() << "\n";
-
 	std::cout << b;
+	char input{};
+	do {
+
+	input = UserInput::getCommandFromUser();
+	std::cout << "You entered: " << input << "\n";
+	} while (input != 'q');
+
+	std::cout << "\n\nBye!!\n\n";
+	return 0;
+
 
 }
